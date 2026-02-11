213bb9e02f900d3969e64ed35dc8a231d85594f8
GennaroBuonomo <gennarobuonomo66@gmail.com>
2026-02-11 17:00:01 +0100

    Push finale: aggiunto database json e definizioni types

---
+++ backend-spec/types.ts
@@ -0,0 +1,11 @@
+export type Product = {
+  title: string;
+  category: string;
+  brand: string;
+  price: number;
+  description: string;
+  image: string;
+  cpu: string;
+  ram: string;
+  storage: string;
+};
